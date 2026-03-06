32e09faa3b203d9bd81623da40274f6f56c8d677
Anderson-Oloroso <henrikog4@gmail.com>
2026-03-06 09:30:40 -0600

    :sparkles: feat: Define Funcion main an menu

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def separador():
     print("="*20)
 
-menu = """      SISTEMA DE INVENTARIO
+def menu():
+    mensaje = """      SISTEMA DE INVENTARIO
     1. Agregar Producto
     2. Listar Productos
     3. Actualizar Cantidad
@@ -9,3 +10,16 @@ menu = """      SISTEMA DE INVENTARIO
     5. Calcular valor total del inventario
     6. Salir
 """
+    print(mensaje)
+    op = int(input("Ingrese una opcion: "))
+
+def main():
+    separador
+    op = menu()
+    separador
+    while True:
+        if op == 1:
+            separador()
+            print("AGREGAR PRODUCTO")
+        if op == 2:
+            print("LISTAR PRODUCTOS")
